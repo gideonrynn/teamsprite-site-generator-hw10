f9a514a721cd6d5809973d910e5af2951aea34d6
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-06 09:07:46 -0600

    added require class_employee to engineer, passed Engineer.test.js

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -9,7 +9,7 @@ const Engineer = require("../lib/class_engineer");
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
-  expect(e.githubEngineer).toBe(testValue);
+  expect(e.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
