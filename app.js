e8767c7e468db3e35bc69fa8a2b119f9b5df7d20
gideonrynn <sarah.c.dillard@gmail.com>
2020-01-08 20:40:58 -0600

    update templates, constructors

--- app.js
+++ app.js
@@ -16,6 +16,19 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const employee = require("./lib/class_employee");
+const manager = require("./lib/class_manager");
+const engineer = require("./lib/class_engineer");
+const intern = require("./lib/class_intern");
+
+//array of data that will be input into arrayHTML
+const arrayAnswers = [];
+
+//array of html that will be created from inputs and collect inputs
+const arrayHTML = [];
+
+//array of html that will be written to the final output team_index.html
+const arrayHTMLFinal = [];
 
 
 function initiate() {
@@ -36,25 +49,146 @@ function initiate() {
       },
       {
         type: "input",
+        name: "id",
+        message: "Enter id:"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter email address (ex/name@domain.com):"
+      },
+      {
+        type: "input",
         name: "username",
         message: "Enter GitHub username:",
         when: (data) => data.type === 'engineer',
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Enter name of school:",
+        when: (data) => data.type === 'intern',
+      },
+      {
+        type: "input",
+        name: "officenumber",
+        message: "Enter office number:",
+        when: (data) => data.type === 'manager',
+      },
+      {
+        type: "confirm",
+        name: "yesorno",
+        message: "Add another team member?"
       }
     ])
 
         //then with the username and color returned from the user's input...
         .then(function(data) {
 
-          console.log(data);
+          // console.log(data);
 
-          initiate();
-          
+          console.log(data.type);
 
+          if (data.type === 'manager') {
+
+            //create new manager with manager class
+            const newManager = new manager(data.name, data.id, data.email, data.officenumber);
+
+            //read contents the html file
+            fs.readFile("./templates/template_manager.html", "utf8", function(err, data) {
+
+              if (err) {
+                throw err;
+              }
+
+              arrayAnswers.push(newManager);
+              arrayHTML.push(data);
+            
+              // console.log(data);
+              //push HTML to array
+              // console.log(arrayHTML);
+              console.log(arrayAnswers);
+
+              writeFile();
+
+            // end read file
+            });
+
+          // end if statement
+          }
+
+          if (data.type === 'intern') {
+            
+            //create new intern with intern class
+            const newIntern = new intern(data.name, data.id, data.email, data.school);
+
+            //read contents the html file
+            fs.readFile("./templates/template_intern.html", "utf8", function(err, data) {
+              
+              if (err) {
+                throw err;
+              }
+              
+              // console.log(data);
+              //push HTML to array
+              arrayHTML.push(data);
+
+              console.log(arrayHTML);
+
+              writeFile();
+
+            // end read file
+            });
+
+          // end if statement
+          }
+
+          if (data.type === 'engineer') {
+           
+            //create new engingeer with engineer class
+            const newEngineer = new engineer(data.name, data.id, data.email, data.github);
+
+            //read contents the html file
+            fs.readFile("./templates/template_engineer.html", "utf8", function(err, data) {
+              
+              if (err) {
+                throw err;
+              }
+              
+              // console.log(data);
+              //push HTML to array
+              arrayHTML.push(data);
+              console.log(arrayHTML);
+              
+              writeFile();
+
+            });
+
+          }
+
+          if (data.yesorno == true) {
+
+            initiate();
+
+          } 
+      
     //end inquirer
      }); 
 
 }
 
+function writeFile() {
+  // write to new file in output folder
+  fs.appendFile("./output/team_index.html", arrayHTML, function(err) {
+  
+    if (err) {
+      return console.log(err);
+    }
+  
+    console.log("Success!");
+  
+    });
+  }
+
 //run when app is initialized
 initiate();
-
